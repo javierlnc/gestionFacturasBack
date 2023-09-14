883df0a543a9fa14c5c71acfbab2851ffeba107f
javierlnc <javierlnc@icloud.com>
2023-09-13 19:49:21 -0500

    Crecion de proyecto

---
+++ src/main/java/co/javierLeon/gestionFacturas/GestionFacturasApplication.java
@@ -0,0 +1,13 @@
+package co.javierLeon.gestionFacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/javierLeon/gestionFacturas/GestionFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.javierLeon.gestionFacturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
